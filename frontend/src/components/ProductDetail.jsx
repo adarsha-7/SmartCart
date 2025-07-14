39b8b0f2e8dab074dee7d75927c1b02eb96fd925
Deep-Neupane <deepbabuneupane@gmail.com>
2025-07-14 19:35:55 +0545

    product detail page minor update

--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -1,117 +1,85 @@
-// import { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-// import axios from 'axios'
-
-// const API_URL =
-//     import.meta.env.VITE_ENV == 'development'
-//         ? import.meta.env.VITE_API_URL_DEV
-//         : import.meta.env.VITE_API_URL
-
-// export default function ProductDetail() {
-//     const { id } = useParams()
-
-//     const [product, setProduct] = useState({})
-
-//     useEffect(() => {
-//         axios
-//             .get(`${API_URL}/api/product?id=${id}`)
-//             .then((res) => setProduct(res.data.product))
-//     }, [])
-
-//     //determine if user is logged in or not, if yes, give "add to cart" option
-
-//     const [login, setLogin] = useState(false)
-
-//     useEffect(() => {
-//         axios
-//             .get(`${API_URL}/api/auth/authentication-test`, {
-//                 withCredentials: true,
-//             })
-//             .then((res) => {
-//                 setLogin(res.data.success)
-//             })
-//             .catch((err) => console.error(err))
-//     }, [])
-
-//     // function for "add to cart" functionality
-
-//     const [message, setMessage] = useState('')
-
-//     function handleAddToCart() {
-//         axios
-//             .post(
-//                 `${API_URL}/api/product/add-to-cart`,
-//                 { productID: id },
-//                 { withCredentials: true }
-//             )
-//             .then((res) => {
-//                 if (res.data.success) {
-//                     setMessage('Product added to Cart Successfully')
-//                     setTimeout(() => {
-//                         setMessage('')
-//                     }, 5000)
-//                 } else {
-//                     setMessage('Failed to add product to cart')
-//                     setTimeout(() => {
-//                         setMessage('')
-//                     }, 5000)
-//                 }
-//             })
-//             .catch((error) => console.error(error))
-//     }
-
-//     const { name, price, image, rating } = product
-
-//     return (
-//         <div>
-//             <div>
-//                 <img src={image}></img>
-//             </div>
-//             <div>
-//                 <div>
-//                     <h2>{name}</h2>
-//                     <p>Rs. {price}</p>
-//                     <p>⭐{rating}</p>
-//                 </div>
-
-//                 <div>Product Specifiaction</div>
-
-//                 <div>Seller Information</div>
-
-//                 {login && (
-//                     <button
-//                         className="cursor-pointer"
-//                         onClick={handleAddToCart}
-//                     >
-//                         Add to Cart
-//                     </button>
-//                 )}
-
-//                 <p className="text-sm text-red-500">{message}</p>
-//             </div>
-//         </div>
-//     )
-// }
-
-import { useParams } from "react-router-dom";
-import items from "./itemsSample";
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 import Navbar from "./Navbar" 
 import Footer from "./Footer"
 import TrendingProducts from "./TrendingProducts";
 import FeaturedProducts from "./FeaturedProducts";
+import Category from './Category'
+
+const API_URL =
+    import.meta.env.VITE_ENV == 'development'
+        ? import.meta.env.VITE_API_URL_DEV
+        : import.meta.env.VITE_API_URL
 
 export default function ProductDetail() {
-    const { id } = useParams(); 
+    const { id } = useParams()
 
-    const product = items.find(p => p.id.toString() === id); 
+    const [product, setProduct] = useState({})
 
-    if (!product) {
-        return <div className="p-4 text-red-600">Product not found</div>;
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/api/product?id=${id}`)
+            .then((res) => setProduct(res.data.product))
+    }, [])
+
+    //determine if user is logged in or not, if yes, give "add to cart" option
+
+    const [login, setLogin] = useState(false)
+
+    useEffect(() => {
+        axios
+            .get(`${API_URL}/api/auth/authentication-test`, {
+                withCredentials: true,
+            })
+            .then((res) => {
+                setLogin(res.data.success)
+            })
+            .catch((err) => console.error(err))
+    }, [])
+
+    // function for "add to cart" functionality
+
+    const [message, setMessage] = useState('')
+
+     const [data, setData] = useState({
+        trendingProducts: [],
+        featuredProducts: [],
+        categories: [],
+    })
+    useEffect(() => {
+        axios.get(`${API_URL}/api/home/content`).then((res) => {
+            setData(res.data)
+        })
+    }, [])
+
+    function handleAddToCart() {
+        axios
+            .post(
+                `${API_URL}/api/product/add-to-cart`,
+                { productID: id },
+                { withCredentials: true }
+            )
+            .then((res) => {
+                if (res.data.success) {
+                    setMessage('Product added to Cart Successfully')
+                    setTimeout(() => {
+                        setMessage('')
+                    }, 5000)
+                } else {
+                    setMessage('Failed to add product to cart')
+                    setTimeout(() => {
+                        setMessage('')
+                    }, 5000)
+                }
+            })
+            .catch((error) => console.error(error))
     }
 
-    const { name, price, image, rating } = product;
+    const { name, price, image, rating } = product
 
     return (
+    
         <>
         <Navbar/>
         <div className="w-402 h-150 bg-white m-10 mt-18 p-10 flex space-x-20">
@@ -148,10 +116,40 @@ export default function ProductDetail() {
             </div>
             
         </div>
-        <TrendingProducts/>
-        <FeaturedProducts/>
+        <TrendingProducts products={data.trendingProducts} />
+        <FeaturedProducts products={data.featuredProducts} />
         <Footer/>
-        </>
+       </>
+
     );
 }
 
+
+
+// <div>
+        //     <div>
+        //         <img src={image}></img>
+        //     </div>
+        //     <div>
+        //         <div>
+        //             <h2>{name}</h2>
+        //             <p>Rs. {price}</p>
+        //             <p>⭐{rating}</p>
+        //         </div>
+
+        //         <div>Product Specifiaction</div>
+
+        //         <div>Seller Information</div>
+
+        //         {login && (
+        //             <button
+        //                 className="cursor-pointer"
+        //                 onClick={handleAddToCart}
+        //             >
+        //                 Add to Cart
+        //             </button>
+        //         )}
+
+        //         <p className="text-sm text-red-500">{message}</p>
+        //     </div>
+        // </d
