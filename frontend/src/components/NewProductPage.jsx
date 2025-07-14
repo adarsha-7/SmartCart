405b22132908cc365d00bc3e8c7ed964926cc85c
s4shresthasujan <158829753+s4shresthasujan@users.noreply.github.com>
2025-07-14 23:47:11 +0545

    Create NewProductPage.jsx

---
+++ frontend/src/components/NewProductPage.jsx
@@ -0,0 +1,14 @@
+import { UserRound } from "lucide-react";
+import Navbar from "./Navbar";
+import NewProductForm from "./NewProductForm";
+
+function NewProductPage() {
+  return (
+    <>
+      <Navbar />
+      <NewProductForm />
+    </>
+  );
+}
+
+export default NewProductPage;
